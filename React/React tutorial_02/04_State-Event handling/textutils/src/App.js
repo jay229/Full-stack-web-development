5bd350281e41b9622f0e533eb845aa792339e0c1
Jay <mritunjaykumartiwari1@gmail.com>
2021-09-18 21:35:27 -0700

    Added button for dark mode in react app TextUtils

--- React/React tutorial_02/04_State-Event handling/textutils/src/App.js
+++ React/React tutorial_02/04_State-Event handling/textutils/src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 
@@ -8,9 +9,10 @@ function App() {
 
   return (
     <>
-      <Navbar title="Textutils" abouttxt="About TextUtils" />
+      <Navbar title="Textutils" abouttxt="About" />
       <div className="container my-3">
         <Textform heading="Enter the Text to analize" />
+        {/* <About/> */}
       </div>
     </>
   );

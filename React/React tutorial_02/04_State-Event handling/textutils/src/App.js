17ef2c2476d614a13e483a2818b88900ccb3638f
Jay <mritunjaykumartiwari1@gmail.com>
2021-09-21 01:19:48 -0700

    React router setup + Usage

--- React/React tutorial_02/04_State-Event handling/textutils/src/App.js
+++ React/React tutorial_02/04_State-Event handling/textutils/src/App.js
@@ -5,32 +5,38 @@ import About from './components/About';
 import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
 
-  const[mode,setMode]=useState("light")
-  const toggleMode=()=>{
-    if(mode==="light"){
+
+  const [mode, setMode] = useState("light")
+  const toggleMode = () => {
+    if (mode === "light") {
       setMode("dark");
-      document.body.style.backgroundColor="#042743";
-      showAlert("Dark mode has enabled","success");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has enabled", "success");
     }
-    else{
+    else {
       setMode("light");
-      document.body.style.backgroundColor="white";
-      showAlert("Light mode has enabled","success");
-      
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has enabled", "success");
+
     }
   }
   const [alert, setAlert] = useState(null)
-  const showAlert=(message,type)=>{
+  const showAlert = (message, type) => {
     setAlert(
       {
-        msg:message,
-        type:type
+        msg: message,
+        type: type
       }
-     
+
 
     )
     setTimeout(() => {
@@ -40,12 +46,27 @@ function App() {
 
   return (
     <>
-      <Navbar title="Textutils" abouttxt="About" mode={mode} toggleMode={toggleMode} />
-      {/* <Alert alert={Alert}/> */}
-      <Alert alert={alert}/>
-      <div className="container my-3">
-        <Textform showAlert={showAlert} heading="Enter the Text to analize" mode={mode} />
-        {/* <About/> */}
+
+
+      <div className=" my-3">
+        <Router>
+          <Navbar title="Textutils" abouttxt="About" mode={mode} toggleMode={toggleMode} />
+          <Alert alert={alert} />
+
+
+          <Switch>
+            <Route exact path="/about">
+              <About />
+            </Route>
+
+            <Route exact path="/">
+              <div className="container">
+
+              <Textform showAlert={showAlert} heading="Enter the Text to analize" mode={mode} />
+              </div>
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </>
   );
--- React/React tutorial_02/04_State-Event handling/textutils/src/components/About.js
+++ React/React tutorial_02/04_State-Event handling/textutils/src/components/About.js
@@ -1,37 +1,36 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 export default function About() {
-    const [myStyle,setmyStyle]=useState(
+    const [myStyle, setmyStyle] = useState(
         {
-            color:'black',
-            backgroundColor:"white"
+            color: 'black',
+            backgroundColor: "white"
         }
     )
-    const[btnText,setBtnText]=useState("Enable Dark mode")
-    const toggleStyle=()=>{
-        if(myStyle.color==='black')
-        {
+    const [btnText, setBtnText] = useState("Enable Dark mode")
+    const toggleStyle = () => {
+        if (myStyle.color === 'black') {
             setmyStyle(
                 {
-                    color:'white',
-                    backgroundColor:"black",
-                    border:'2px solid white'
-                    
+                    color: 'white',
+                    backgroundColor: "black",
+                    border: '2px solid white'
+
                 }
             )
             setBtnText("Enable Light mode");
         }
-        else{
+        else {
             setmyStyle(
                 {
-                    color:'black',
-                    backgroundColor:"white"
+                    color: 'black',
+                    backgroundColor: "white"
                 }
             )
             setBtnText("Enable Dark mode");
         }
     }
-    
+
     return (
         <div className="container my-3" style={myStyle}>
             <h1 >About Us</h1>
@@ -50,7 +49,7 @@ export default function About() {
                 </div>
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingTwo">
-                        <button className="accordion-button collapsed"style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                             Accordion Item #2
                         </button>
                     </h2>
@@ -62,18 +61,18 @@ export default function About() {
                 </div>
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingThree">
-                        <button className="accordion-button collapsed"style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                             Accordion Item #3
                         </button>
                     </h2>
                     <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                        <div className="accordion-body"style={myStyle}>
+                        <div className="accordion-body" style={myStyle}>
                             <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
                         </div>
                     </div>
                 </div>
             </div>
-            <button type="button" onClick={toggleStyle}  class="btn btn-primary my-3">{btnText}</button>
+            <button type="button" onClick={toggleStyle} className="btn btn-primary my-3">{btnText}</button>
         </div>
     )
 }

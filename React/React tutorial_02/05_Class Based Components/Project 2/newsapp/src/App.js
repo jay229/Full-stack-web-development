dcbf848f3106a59be8360ec237f96216f7353b78
Jay <mritunjaykumartiwari1@gmail.com>
2021-09-24 04:03:31 -0700

    Component structure of our NewsMonkey react app

--- React/React tutorial_02/05_Class Based Components/Project 2/newsapp/src/App.js
+++ React/React tutorial_02/05_Class Based Components/Project 2/newsapp/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-
 import React, { Component } from 'react'
+import NavBar from './Components/NavBar';
+import News from './Components/News';
 
 export default class App extends Component {
   c="class based component."
   render() {
     return (
       <div>
-        Hello I'm a {this.c}
+        <NavBar/>
+        <News/>
       </div>
     )
   }

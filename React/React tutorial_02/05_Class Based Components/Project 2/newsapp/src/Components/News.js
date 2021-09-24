82a8eef3fa17c635b8b2578dcae0e2984b0fe9dd
Jay <mritunjaykumartiwari1@gmail.com>
2021-09-24 04:46:06 -0700

    Fetching API Key from News API

--- React/React tutorial_02/05_Class Based Components/Project 2/newsapp/src/Components/News.js
+++ React/React tutorial_02/05_Class Based Components/Project 2/newsapp/src/Components/News.js
@@ -4,13 +4,29 @@ import NewsItem from './NewsItem'
 export class News extends Component {
     render() {
         return (
-            <div>
-                This is News component.
-                <NewsItem/>
-                <NewsItem/>
-                <NewsItem/>
-                <NewsItem/>
-                <NewsItem/>
+            <div className="container my-3" >
+                <h2>NewsMonkey-Top Headlines</h2>
+                <div className="row">
+                    <div className="col-md-4">
+                        <NewsItem title="News title" description="News description" />
+                        <NewsItem title="News title" description="News description" />
+                        <NewsItem title="News title" description="News description" />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title="News title" description="News description" />
+                        <NewsItem title="News title" description="News description" />
+                        <NewsItem title="News title" description="News description" />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title="News title" description="News description" />
+                        <NewsItem title="News title" description="News description" />
+                        <NewsItem title="News title" description="News description" />
+                    </div>
+                </div>
+
+
+
+
             </div>
         )
     }

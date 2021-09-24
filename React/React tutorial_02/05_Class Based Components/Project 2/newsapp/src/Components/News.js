08b30bc3634c3e4d0d57ba9c8358dbd162536251
Jay <mritunjaykumartiwari1@gmail.com>
2021-09-24 09:11:23 -0700

    Looping through an array in JSX to display NewsItems from state

--- React/React tutorial_02/05_Class Based Components/Project 2/newsapp/src/Components/News.js
+++ React/React tutorial_02/05_Class Based Components/Project 2/newsapp/src/Components/News.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-    articles=[
+    articles = [
         {
             "source": {
-            "id": "bbc-news",
-            "name": "BBC News"
+                "id": "bbc-news",
+                "name": "BBC News"
             },
             "author": "BBC News",
             "title": "Jitender Maan Gogi: India gangster shot dead in Delhi court",
@@ -15,11 +15,11 @@ export class News extends Component {
             "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/180CC/production/_120680589_gettyimages-1192193014.jpg",
             "publishedAt": "2021-09-24T11:07:21.5956522Z",
             "content": "image source, Getty Images\r\nimage captionThe incident has raised questions over the security of Delhi's courts\r\nTwo men posing as lawyers shot dead a notorious gangster inside a courtroom in India's … [+944 chars]"
-            },
-            {
+        },
+        {
             "source": {
-            "id": "bbc-news",
-            "name": "BBC News"
+                "id": "bbc-news",
+                "name": "BBC News"
             },
             "author": "BBC News",
             "title": "Afghanistan: Executions will return, says senior Taliban official",
@@ -28,11 +28,11 @@ export class News extends Component {
             "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/4701/production/_120677181_mediaitem120677180.jpg",
             "publishedAt": "2021-09-24T11:07:19.2212422Z",
             "content": "image captionMullah Nooruddin Turabi, not pictured, said punishments such as amputations were \"necessary\"\r\nThe Taliban's notorious former head of religious police has said extreme punishments such as… [+3564 chars]"
-            },
-            {
+        },
+        {
             "source": {
-            "id": "bbc-news",
-            "name": "BBC News"
+                "id": "bbc-news",
+                "name": "BBC News"
             },
             "author": "BBC News",
             "title": "CIA 'removes Vienna boss' over Havana syndrome outbreak",
@@ -41,11 +41,11 @@ export class News extends Component {
             "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/B849/production/_120677174_gettyimages-520956552.jpg",
             "publishedAt": "2021-09-24T10:37:27.1034726Z",
             "content": "image captionDozens of US personnel have been affected by the mysterious illness in Vienna, US media report\r\nThe CIA has reportedly removed its station chief in Vienna for not responding appropriatel… [+1993 chars]"
-            },
-            {
+        },
+        {
             "source": {
-            "id": "bbc-news",
-            "name": "BBC News"
+                "id": "bbc-news",
+                "name": "BBC News"
             },
             "author": "BBC News",
             "title": "New Zealand council's Zoom talks go viral as pretend meeting",
@@ -54,11 +54,11 @@ export class News extends Component {
             "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/1774D/production/_120677069_zoom2.png",
             "publishedAt": "2021-09-24T10:37:23.6355856Z",
             "content": "image source, Waipa District Council/YouTube\r\nimage captionThe council described the Zoom footage, recorded last year, as \"innocuous\"\r\nA New Zealand council's Zoom meeting has been viewed more than 2… [+1746 chars]"
-            },
-            {
+        },
+        {
             "source": {
-            "id": "bbc-news",
-            "name": "BBC News"
+                "id": "bbc-news",
+                "name": "BBC News"
             },
             "author": "BBC News",
             "title": "Haitian migrants at US border: 'We've been through 11 countries'",
@@ -67,11 +67,11 @@ export class News extends Component {
             "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/BC27/production/_120676184_fitersonpic.jpg",
             "publishedAt": "2021-09-24T10:37:19.3397791Z",
             "content": "By Will GrantBBC News, Mexicali, Mexico \r\nimage caption\"This world doesn't want to see us,\" Fiterson Janvier says\r\nEven in late summer, few migrants attempt to cross into the United States from the M… [+4200 chars]"
-            },
-            {
+        },
+        {
             "source": {
-            "id": "bbc-news",
-            "name": "BBC News"
+                "id": "bbc-news",
+                "name": "BBC News"
             },
             "author": "BBC News",
             "title": "George Floyd murder: Derek Chauvin appeals against conviction",
@@ -80,11 +80,11 @@ export class News extends Component {
             "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/1114F/production/_120676996_hi061778987.jpg",
             "publishedAt": "2021-09-24T10:22:25.3852762Z",
             "content": "image captionDerek Chauvin (R) listens to his sentencing in June alongside his defence lawyer Eric Nelson\r\nThe former Minneapolis police officer convicted of the murder of African-American man George… [+1321 chars]"
-            },
-            {
+        },
+        {
             "source": {
-            "id": "bbc-news",
-            "name": "BBC News"
+                "id": "bbc-news",
+                "name": "BBC News"
             },
             "author": "BBC News",
             "title": "Kim Yo-jong says North Korea open to ending war if conditions met",
@@ -93,11 +93,11 @@ export class News extends Component {
             "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/9C83/production/_120676004_gettyimages-916165588.jpg",
             "publishedAt": "2021-09-24T07:52:26.4882068Z",
             "content": "image source, Getty Images\r\nimage captionKim Yo-jong is the influential sister of North Korean leader Kim Jong-un\r\nThe influential sister of North Korean leader Kim Jong-un says they are willing to r… [+3327 chars]"
-            },
-            {
+        },
+        {
             "source": {
-            "id": "bbc-news",
-            "name": "BBC News"
+                "id": "bbc-news",
+                "name": "BBC News"
             },
             "author": "BBC News",
             "title": "Catalan separatist Carles Puigdemont arrested in Italy",
@@ -106,11 +106,11 @@ export class News extends Component {
             "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/140F7/production/_120676128_puigdemont.jpg",
             "publishedAt": "2021-09-24T04:22:22.4321316Z",
             "content": "image captionCarles Puigdemont has been living in self-imposed exile in Belgium\r\nCatalan separatist leader Carles Puigdemont has been detained in Sardinia on a Spanish arrest warrant. \r\nSpain has acc… [+1717 chars]"
-            },
-            {
+        },
+        {
             "source": {
-            "id": "bbc-news",
-            "name": "BBC News"
+                "id": "bbc-news",
+                "name": "BBC News"
             },
             "author": "BBC News",
             "title": "Why are so many Haitians at the US-Mexico border?",
@@ -119,11 +119,11 @@ export class News extends Component {
             "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/1853F/production/_120674699_riogrande.jpg",
             "publishedAt": "2021-09-24T01:22:24.9297636Z",
             "content": "By Bernd Debusmann JrBBC News, Washington\r\nimage captionMigrants have been crossing the Rio Grande river from Mexico to reach the US\r\nThousands of predominantly Haitian migrants are still camped at t… [+4423 chars]"
-            },
-            {
+        },
+        {
             "source": {
-            "id": "bbc-news",
-            "name": "BBC News"
+                "id": "bbc-news",
+                "name": "BBC News"
             },
             "author": "BBC News",
             "title": "Freezing to death on the edge of the EU",
@@ -132,14 +132,14 @@ export class News extends Component {
             "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/FB65/production/_120675346_p09wtl4l.jpg",
             "publishedAt": "2021-09-24T00:37:21.5556966Z",
             "content": "The BBC has obtained first-hand accounts from migrants who say theyve been illegally deported from the European Union by Polish border troops. \r\nClose to the border between Belarus and Poland, the BB… [+561 chars]"
-            }
+        }
     ]
-    constructor(){
+    constructor() {
         super();
         // console.log("Hii");
-        this.state={
-            articles:this.articles,
-            loading:false
+        this.state = {
+            articles: this.articles,
+            loading: false
         }
 
     }
@@ -148,22 +148,18 @@ export class News extends Component {
             <div className="container my-3" >
                 <h2>NewsMonkey-Top Headlines</h2>
                 <div className="row">
-                    <div className="col-md-4">
-                        <NewsItem title="News title" description="News description" imageUrl="https://ichef.bbci.co.uk/news/1024/branded_news/180CC/production/_120680589_gettyimages-1192193014.jpg" />
-                        <NewsItem title="News title" description="News description" />
-                        <NewsItem title="News title" description="News description" />
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem title="News title" description="News description" />
-                        <NewsItem title="News title" description="News description" />
-                        <NewsItem title="News title" description="News description" />
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem title="News title" description="News description" />
-                        <NewsItem title="News title" description="News description" />
-                        <NewsItem title="News title" description="News description" />
-                    </div>
+                    {this.state.articles.map(
+                        (element) => {
+                            return <div className="col-md-4" key={element.url}>
+                                <NewsItem title={element.title} description={element.title} imageUrl={element.urlToImage} newsUrl={element.url} />
+
+                            </div>
+
+
+                        }
+                    )}
                 </div>
+
 
 
 

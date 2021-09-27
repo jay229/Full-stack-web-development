c645cdf9fc5475efc1ad14751cc6c8fcb23d8496
Jay <mritunjaykumartiwari1@gmail.com>
2021-09-27 08:09:30 -0700

    Next button issue resolve

--- React/React tutorial_02/05_Class Based Components/Project 2/newsapp/src/Components/News.js
+++ React/React tutorial_02/05_Class Based Components/Project 2/newsapp/src/Components/News.js
@@ -9,26 +9,35 @@ export class News extends Component {
         this.state = {
             articles: [],
             loading: false,
-            page: 1
+            page: 1,
+            totalResults:0
+           
+            
         }
 
     }
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=a64092eee06b4808b7a20c9f8e14849d&page=1&pageSize=4"
+        let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=a64092eee06b4808b7a20c9f8e14849d&page=1&pageSize=12"
         let data = await fetch(url);
         let parseData = await data.json();
         //  console.log(parseData);
-        this.setState({ articles: parseData.articles , totalResults:parseData.totalResults})
+        this.setState({articles: parseData.articles,totalResults:parseData.totalResults})
+
+        // console.log(parseData.totalResults);
+        // console.log(this.state.totalResults);
     }
     nextPage = async () => {
-       if(this.state.page+1>(Math.ceil(this.totalResults/4)))
-       {
+        // console.log(this.state.totalResults);
+        console.log((Math.ceil(this.state.totalResults/12)));
+        console.log(this.state.page);
+        if(this.state.page+1>(Math.ceil(this.state.totalResults/12)))
+        {
         console.log("Hii")
        }
         else
         {
 
-            let url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=a64092eee06b4808b7a20c9f8e14849d&page= ${this.state.page+1}&pageSize=4`;
+            let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=a64092eee06b4808b7a20c9f8e14849d&page= ${this.state.page+1}&pageSize=12`;
             let data = await fetch(url);
             let parseData = await data.json();
             this.setState(
@@ -40,7 +49,7 @@ export class News extends Component {
         }
     }
     prevPage = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=a64092eee06b4808b7a20c9f8e14849d&page= ${this.state.page-1}&pageSize=4`;
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=a64092eee06b4808b7a20c9f8e14849d&page= ${this.state.page-1}&pageSize=12`;
         let data = await fetch(url);
         let parseData = await data.json();
         this.setState(
@@ -67,7 +76,7 @@ export class News extends Component {
                     )}
                     <div className="container d-flex justify-content-between">
                         <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.prevPage}>&larr;Previous</button>
-                        <button type="button" className="btn btn-dark" onClick={this.nextPage}>Next&rarr;</button>
+                        <button disabled={this.state.page+1>(Math.ceil(this.state.totalResults/12))} type="button" className="btn btn-dark" onClick={this.nextPage}>Next&rarr;</button>
                     </div>
                 </div>
 

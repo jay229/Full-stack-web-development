e197cbe1e6590baa48acc99e35c448fd1cd7d114
MRITUNJAY <mritunjaykumartiwari1@gmail.com>
2021-10-25 16:36:34 +0530

    nodemon is installed

---
+++ NodeJs/07_Nodemon/index.js
@@ -0,0 +1,8 @@
+/*nodemon:nodemon is a tool that helps develop node.js based applications by automatically restarting the node application when file changes in the directory are detected.
+>Install nodemon by typing npm i nodemon in terminal or cmd .
+*/
+let a=10;
+let b=40;
+console.log(a+b);
+
+// For running js code using nodemon type in terminal : nodemon filename

a737e5bb7cdcae44a72d55b2266c81ec06cf5c27
MRITUNJAY <mritunjaykumartiwari1@gmail.com>
2021-10-25 10:52:29 +0530

    Creating own module and use it

---
+++ NodeJs/05_CreateExportModule/index.js
@@ -0,0 +1,9 @@
+// const md=require("./module");
+// console.log(md.add(5,5));
+// console.log(md.sub(5,5));
+// console.log(md.mul(5,5));
+
+const {add,sub,mul}=require("./module");
+console.log(add(5,5));
+console.log(sub(5,5));
+console.log(mul(5,5))

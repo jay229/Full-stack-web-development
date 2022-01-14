7f60b840fd2c02bd269fc2764e35d956ec1eeb66
MRITUNJAY <mritunjaykumartiwari1@gmail.com>
2022-01-14 20:05:54 +0530

    POST method with async-await

--- 09_MongoDB-Express/13_RESTAPI/01_RESTapiPOST/src/app.js
+++ 09_MongoDB-Express/13_RESTAPI/01_RESTapiPOST/src/app.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const express = require("express")
 require("./db/conn")
 const Student = require("./model/students")
@@ -8,14 +9,23 @@ app.use(express.json())
 // app.get("",(req,res)=>{
 
 // })
-app.post("/students", (req, res) => {
-    // res.send("Hello from another side...")
-    const student1=new Student(req.body)
-    console.log(req.body)
-    student1. save()
-    .then(()=>res.send(student1))
-    .catch((e)=>res.send(e))
-    
+// app.post("/students", (req, res) => {
+   
+//     const student1=new Student(req.body)
+//     console.log(req.body)
+//     student1. save()
+//     .then(()=>res.send(student1))
+//     .catch((e)=>res.send(e))
+// })
+
+app.post("/students",async (req,res)=>{
+    try {
+        const student=new Student(req.body)
+        const result=await student.save()
+        res.status(201).send(result)
+    } catch (error) {
+        res.status(400).send(e)
+    }
 })
 app.listen(port, () => {
     console.log(`Server is setup at ${port}`)
